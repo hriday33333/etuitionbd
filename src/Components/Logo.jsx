00f82ddc4aaec20a9c4b653e03fc7f6dbedf4b91
hriday33333 <zakirhasanhriday@gmail.com>
2025-12-07 23:35:05 +0600

    added navber and footer

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,9 +1,9 @@
-import logo from '../assets/logo-2.png';
+import logo from '../assets/logo1.png';
 const Logo = () => {
   return (
     <div className='w-[75px] flex items-end  '>
       <img src={logo} alt="" />
-      <h1 className='font-bold text-xl -ms-1 mb-2'>eTuition</h1>
+      <h1 className='font-bold text-primary text-xl -ms-1 mb-2'>eTuition</h1>
     </div>
   );
 };
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import Logo from "../../../Components/Logo";
+import { NavLink } from 'react-router';
+import Logo from '../../../Components/Logo';
 
 const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to="">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="">Tuitions</NavLink>
+      </li>
+      <li>
+        <NavLink to="">Tuitions</NavLink>
+      </li>
+      <li>
+        <NavLink to="">Tuitions</NavLink>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className="ms-3 btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -26,54 +43,20 @@ const Navbar = () => {
             tabIndex="-1"
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {links}
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">
-          <Logo></Logo>
+          <div className="-ms-5">
+            <Logo></Logo>
+          </div>
         </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100 w-40 z-1">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{links}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn bg-primary text-secondary">Button</a>
       </div>
     </div>
   );
